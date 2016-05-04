42144576430d88a64181036095a8eb8b3ab20ef0
Jonathan Bowers <jonotron@gmail.com>
2016-05-04 14:20:01 -0700

    comment why we replace %and etc

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var parser = require('./jouch').parser
 function parse (input) {
   var parsed = parser.parse(input)
 
+  // jouch returns %and, %or and %not due some jison incompatibility
+  // we need to replace them here and then parse as JSON
   parsed = parsed.replace('\%and', '$and')
   parsed = parsed.replace('\%or',  '$or')
   parsed = parsed.replace('\%not', '$not')
